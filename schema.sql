a1eada760eef4916fb32e132dc5d47597fb7fb4b
tonnytech <tonnytei4@gmail.com>
2023-08-14 16:40:27 +0300

    added animals table

---
+++ schema.sql
@@ -0,0 +1,10 @@
+/* Database schema to keep the structure of entire database. */
+CREATE TABLE animals (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name CHAR(255),
+    date_of_birth DATE,
+    escape_attempts INT,
+    neutered BIT,
+    weight_kg DECIMAL(5, 2),
+    PRIMARY KEY(id)
+);
