dad813cc1d00e0c65dda59a633c9e4c6a47d4a0c
tonnytech <tonnytei4@gmail.com>
2023-08-19 13:35:34 +0300

    added querries to multiple tables

--- queries.sql
+++ queries.sql
@@ -114,7 +114,7 @@ SELECT
 FROM
     animals;
 
--- Inside a transaction
+-- Inside a transactionModify animals table
 BEGIN;
 
 -- delete all records in the animals table
@@ -217,3 +217,63 @@ WHERE
     AND '2000-12-31'
 GROUP BY
     species;
+
+-----------------------------------------------------------------------
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * ---------------------------------------------------------------------
+-- What animals belong to Melody Pond?
+SELECT
+    owners.full_name AS owner,
+    animals.name AS animal
+FROM
+    animals
+    JOIN owners ON animals.owner_id = owners.id
+WHERE
+    owners.full_name = 'Melody Pond';
+
+-- List of all animals that are pokemon (their type is Pokemon).
+SELECT
+    animals.name AS animal_name,
+    species.name AS species_name
+FROM
+    animals
+    JOIN species ON animals.species_id = species.id
+WHERE
+    species.name = 'Pokemon';
+
+-- List all owners and their animals, remember to include those that don't own any animal.
+SELECT
+    owners.full_name AS owner,
+    animals.name AS animal
+FROM
+    owners
+    LEFT JOIN animals ON owners.id = animals.owner_id;
+
+-- How many animals are there per species?
+SELECT
+    species.name AS species,
+    COUNT(animals.id) AS count
+FROM
+    species
+    LEFT JOIN animals ON species.id = animals.species_id
+GROUP BY
+    species.name;
+
+-- List all Digimon owned by Jennifer Orwell.
+SELECT animals.name AS animal, species.name AS species
+FROM animals
+JOIN species ON animals.species_id = species.id
+JOIN owners ON animals.owner_id = owners.id
+WHERE species.name = 'Digimon' AND owners.full_name = 'Jennifer Orwell';
+-- List all animals owned by Dean Winchester that haven't tried to escape.
+SELECT animals.name AS animal, animals.escape_attempts
+FROM animals
+JOIN owners ON animals.owner_id = owners.id
+WHERE owners.full_name = 'Dean Winchester' AND animals.escape_attempts <= 0;
+
+-- Who owns the most animals?
+SELECT owners.full_name AS owner, COUNT(animals.id) AS count
+FROM owners
+LEFT JOIN animals ON owners.id = animals.owner_id
+GROUP BY owners.id, owners.full_name
+ORDER BY count DESC
+LIMIT 1;
--- schema.sql
+++ schema.sql
@@ -13,3 +13,59 @@ ALTER TABLE
     animals
 ADD
     species VARCHAR(255);
+
+-- Create a table named owners
+CREATE TABLE owners (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    full_name VARCHAR(256),
+    age INT,
+    PRIMARY KEY (id)
+);
+
+-- Create a table named species
+CREATE TABLE species (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(250),
+    PRIMARY KEY(id)
+);
+
+-- Modify animals table make sure that id is set as autoincremented PRIMARY KEY
+ALTER TABLE
+    animals
+ALTER COLUMN
+    id
+ADD
+    GENERATED ALWAYS AS IDENTITY;
+
+-- Remove column species
+ALTER TABLE
+    animals DROP COLUMN species;
+
+-- Add column species_id which is a foreign key referencing species table
+ALTER TABLE
+    animals
+ADD
+    COLUMN species_id INT;
+
+ALTER TABLE
+    animals
+ADD
+    CONSTRAINT fk_species FOREIGN KEY (species_id) REFERENCES species(id);
+
+ALTER TABLE
+    animals
+ADD
+    COLUMN owner_id INT;
+
+ALTER TABLE
+    animals
+ADD
+    CONSTRAINT fk_owner FOREIGN KEY (owner_id) REFERENCES owners(id);
+
+-- Add column owner_id which is a foreign key referencing the owners table
+ALTER TABLE
+    animals
+ADD
+    COLUMN owner_id INT,
+ADD
+    CONSTRAINT fk_owner FOREIGN KEY (owner_id) REFERENCES owners(id);
